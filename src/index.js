4abd9d53c40ac9f6e18418079eb15d55b0e07922
Toma Khliabets <tokotomka@gmail.com>
2019-02-20 15:22:09 +0300

    Finish task

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    let lovers = 0, i;
+    for(i = 0; i < preferences.length; i++) {
+        let triangle = [], h = 0, j = i + 1;
+        while(h < 3) {
+            triangle.push(j);
+            j = preferences[j - 1];
+            h++;
+        }
+        let trueTriangle = triangle.filter((e, i, arr) => arr.indexOf(e) === arr.lastIndexOf(e));
+        if(trueTriangle.length === 3 && triangle[0] === j) lovers +=1;
+    }
+    return lovers / 3;
 };
